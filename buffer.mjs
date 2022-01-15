d6bcbf1ab070e29b864afeff387328f5a8f55e99
aircode91 <firmanagus59@gmail.com>
2022-01-16 00:26:37 +0700

    add buffer

---
+++ buffer.mjs
@@ -0,0 +1,7 @@
+const buffer = Buffer.from("Izza Hilyah Nafisah");
+
+console.info(buffer);
+const data= buffer.toString();
+console.info(`Hallo ${data}`)
+const dataReverse= buffer.reverse();
+console.info(`Hallo ${dataReverse}`)
